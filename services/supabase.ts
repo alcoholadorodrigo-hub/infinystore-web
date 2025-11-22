8951093f82a40bb78627a216ad42f06189a17618
alcoholadorodrigo-hub <alcoholadorodrigo@gmail.com>
2025-11-21 22:47:23 -0300

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,8 +1,8 @@
+// services/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
-export default supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
